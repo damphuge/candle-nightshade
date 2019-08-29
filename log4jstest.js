e1d99bd882be2d07840f9242cd88d6ff9352f29b
Glitch (candle-nightshade) <none>
2019-08-29 17:40:55 +0000

    🍠👙 Checkpoint ./log4jstest.js:6409424/1483

--- log4jstest.js
+++ log4jstest.js
@@ -21,27 +21,38 @@ logger.info('Hello world!');
 
 
 /*
-https://www.drk7.jp/weather/
-// > node.jsでタイムゾーンの変換処理にdate-fns-timezoneを利用する - Qiita
-// > https://qiita.com/kazuhiro1982/items/b1235a893ee874d8ff65
-// import
-const { convertToTimeZone } = require('date-fns-timezone');
-
-// タイムゾーン定義
-const timeZone = "Asia/Tokyo";
-// 現在時刻(UTC)を取得
-const targetDate = new Date();
-// TimeZone付きDateに変換
-const zonedTargetDate = convertToTimeZone(targetDate, { timeZone: timeZone });
-// 年月日を取得
-const year = zonedTargetDate.getFullYear();
-const month = zonedTargetDate.getMonth() + 1;
-const day = zonedTargetDate.getDate();
-// 書式をYYYY/MM/DDに変換
-// > JavaScriptでゼロパディングして桁をそろえる方法
-// > https://so-zou.jp/web-app/tech/programming/javascript/grammar/data-type/string/zero-padding.htm
-const date = year + '/' +
-      ('00' + month).slice(-2) + '/' +
-      ('00' + day).slice(-2);
-// 
-
+  今日の日付を取得する。
+  天気情報サイトのxmlファイル中の日付の書式に合わせる。
+  - サイト: https://www.drk7.jp/weather/
+  
+  return: date (String)
+  
+  ref:
+  - node.jsでタイムゾーンの変換処理にdate-fns-timezoneを利用する - Qiita
+    - https://qiita.com/kazuhiro1982/items/b1235a893ee874d8ff65
+  - JavaScriptでゼロパディングして桁をそろえる方法
+    - https://so-zou.jp/web-app/tech/programming/javascript/grammar/data-type/string/zero-padding.htm
+*/
+var getToday = function(relative) {
+  // import
+  const { addDays } = require('date-fns');
+  const { convertToTimeZone } = require('date-fns-timezone');
+  // タイムゾーン定義
+  const timeZone = "Asia/Tokyo";
+  // 現在時刻(UTC)を取得
+  const targetDate = new Date();
+  // TimeZone付きDateに変換
+  const zonedTargetDate = convertToTimeZone(targetDate, { timeZone: timeZone });
+  
+  
+  // 年月日を取得
+  const year = zonedTargetDate.getFullYear();
+  const month = zonedTargetDate.getMonth() + 1;
+  const day = zonedTargetDate.getDate();
+  // 書式をYYYY/MM/DDに変換
+  const date = year + '/' +
+        ('00' + month).slice(-2) + '/' +
+        ('00' + day).slice(-2);
+  
+  return date;
+};
