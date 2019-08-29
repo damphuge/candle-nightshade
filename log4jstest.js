50eb5912a9bc0178283fb7239dbacfeea3e90670
Glitch (candle-nightshade) <none>
2019-08-29 17:28:55 +0000

    🚟🚗 Checkpoint ./log4jstest.js:6409424/1038

--- log4jstest.js
+++ log4jstest.js
@@ -18,23 +18,30 @@ const logger = log4js.getLogger();
 
 logger.info('Hello world!');
 
-// node.jsでタイムゾーンの変換処理にdate-fns-timezoneを利用する - Qiita: https://qiita.com/kazuhiro1982/items/b1235a893ee874d8ff65
+
+
+/*
+https://www.drk7.jp/weather/
+// > node.jsでタイムゾーンの変換処理にdate-fns-timezoneを利用する - Qiita
+// > https://qiita.com/kazuhiro1982/items/b1235a893ee874d8ff65
 // import
-const { startOfDay, addDays } = require('date-fns');
 const { convertToTimeZone } = require('date-fns-timezone');
 
 // タイムゾーン定義
 const timeZone = "Asia/Tokyo";
-
 // 現在時刻(UTC)を取得
 const targetDate = new Date();
 // TimeZone付きDateに変換
 const zonedTargetDate = convertToTimeZone(targetDate, { timeZone: timeZone });
-// 文字列化
-const d1 = zonedTargetDate.toISOString(); // => '2019-03-05T15:00:00.000Z'
-
-logger.info(d1);
+// 年月日を取得
+const year = zonedTargetDate.getFullYear();
+const month = zonedTargetDate.getMonth() + 1;
+const day = zonedTargetDate.getDate();
+// 書式をYYYY/MM/DDに変換
+// > JavaScriptでゼロパディングして桁をそろえる方法
+// > https://so-zou.jp/web-app/tech/programming/javascript/grammar/data-type/string/zero-padding.htm
+const date = year + '/' +
+      ('00' + month).slice(-2) + '/' +
+      ('00' + day).slice(-2);
 // 
-
-var 
 
