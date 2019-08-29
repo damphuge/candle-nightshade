7cc4a3aaf767387d78d3d789bce764a87a954d18
Glitch (candle-nightshade) <none>
2019-08-29 16:16:54 +0000

    ❣️💳 Checkpoint ./log4jstest.js:6409424/705

--- log4jstest.js
+++ log4jstest.js
@@ -1,6 +1,16 @@
 'use strict'
 const log4js = require('log4js')
-const logger = log4js.getLogger();
-logger.level = 'debug';
- 
+log4js.configure({
+  appenders: {
+    log: {type:'file', filename:'system.log'},
+    out: {type:'stdout'}
+  },
+  categories: {
+    default: {appenders: ['out'], level: 'warn'},
+    develop: {appenders: ['log'], level: 'debug'}
+  }
+})
+
+const logger = log4js.getLogger('develop');
+
 logger.debug('Hello world!');
