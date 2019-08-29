a668fc4bc1b6842da48895071bdec61ca17be3b7
Glitch (candle-nightshade) <none>
2019-08-29 08:41:59 +0000

    🏗🌖 Checkpoint ./main.js:6189558/1063

--- main.js
+++ main.js
@@ -121,3 +121,29 @@ if(process.env.DISCORD_BOT_TOKEN == undefined)
 }
 
 client.login( process.env.DISCORD_BOT_TOKEN );
+
+var request = require('request');
+var parseString = require('xml2js').parseString;
+ 
+var url = 'http://www.drk7.jp/weather/xml/02.xml';
+ //
+// request(url, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         parseString(body, function (err, result) {
+ 
+//             var day = result.weatherforecast.pref[0].area[3].info[0]['$'].date + "\n";
+//             var weather = result.weatherforecast.pref[0].area[3].info[0].weather[0] + "\n";
+//             var detail = result.weatherforecast.pref[0].area[3].info[0].weather_detail[0] + "\n";
+//             var max = "最高気温は" + result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[0]._ + "度\n";
+//             var min = "最低気温は" + result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[1]._ + "度です";
+ 
+//             var message = "おはようございます。天気予報です\n" + day + weather + detail + max + min;
+ 
+//             console.log(message);
+ 
+//         });
+//     } else {
+//         console.log(error + " : " + response);
+//     }
+ 
+// });
