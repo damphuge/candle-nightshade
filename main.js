06b333174f8a50798cdb7865ea389597e8ea7d30
Glitch (candle-nightshade) <none>
2019-08-29 20:18:29 +0000

    😽🌄 Checkpoint ./main.js:6409424/447

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 //おめシスはいいぞ
 //おめシスはいいぞ
 
-// Logger, Dateなどのやつ
+// Dateなどのやつ
 let util = require('./utilities.js');
 
 // Response for Uptime Robot
@@ -39,16 +39,19 @@ var url = 'https://www.drk7.jp/weather/xml/' + prefAomori + '.xml';
 request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         parseString(body, function (err, result) {
-          if(result.weatherforecast.pref[0].area[areaTsugaru].info[0]['$'].date === today)
-            var day = result.weatherforecast.pref[0].area[areaTsugaru].info[today]['$'].date + "\n";
-            var weather = result.weatherforecast.pref[0].area[areaTsugaru].info[today].weather[0] + "\n";
-            var detail = result.weatherforecast.pref[0].area[areaTsugaru].info[0].weather_detail[0] + "\n";
-            var max = "最高気温は" + result.weatherforecast.pref[0].area[areaTsugaru].info[0].temperature[0].range[0]._ + "度ハメ\n";
-            var min = "最低気温は" + result.weatherforecast.pref[0].area[areaTsugaru].info[0].temperature[0].range[1]._ + "度ですハメ";
+          // [現時点では]今日の日付に一致する天気を取得。
+          for(let i=0; i < 6; i++) {
+            if(result.weatherforecast.pref[0].area[areaTsugaru].info[i]['$'].date === today){
+              var day = result.weatherforecast.pref[0].area[areaTsugaru].info[i]['$'].date + "\n";
+              var weather = result.weatherforecast.pref[0].area[areaTsugaru].info[i].weather[0] + "\n";
+              var detail = result.weatherforecast.pref[0].area[areaTsugaru].info[i].weather_detail[0] + "\n";
+              var max = "最高気温は" + result.weatherforecast.pref[0].area[areaTsugaru].info[i].temperature[0].range[0]._ + "度ハメ\n";
+              var min = "最低気温は" + result.weatherforecast.pref[0].area[areaTsugaru].info[i].temperature[0].range[1]._ + "度ですハメ";
+            }
+          }
+          tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
  
-            tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
- 
-            console.log(tenki);
+          console.log(tenki);
  
         });
     } else {
