bc5fb224833af0177e777dbd9b23b7bd3d023492
Glitch (candle-nightshade) <none>
2019-08-30 03:56:37 +0000

    🐛🍀 Checkpoint ./weatherRoidTypeA.js:6409424/997

--- weatherRoidTypeA.js
+++ weatherRoidTypeA.js
@@ -6,35 +6,37 @@
   @param String $pref 都道府県(初期値は青森県), xmlファイル名の番号
 */
 let getWeather = function(date, area=2, pref='02'){
-  let request = require('request');
-  let parseString = require('xml2js').parseString;
-  
-
-  //ここがXMLパーサ
-  const promise = new Primise((request, reject) => {
+  const p = new Promise((resolve, reject) => {
+    let request = require('request');
+    let parseString = require('xml2js').parseString;
+    let url = 'https://www.drk7.jp/weather/xml/' + pref + '.xml';
+    //ここがXMLパーサ
     request (url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      parseString(body, function (err, result) {
-      // [現時点では]今日の日付に一致する天気を取得。
-        console.log('unchi' + date);
-        for(let i=0; i < 6; i++) {
-          if(result.weatherforecast.pref[0].area[area].info[i]['$'].date === date){
-            console.log(date);
-            var day = result.weatherforecast.pref[0].area[area].info[i]['$'].date + "\n";
-            var weather = result.weatherforecast.pref[0].area[area].info[i].weather[0] + "\n";
-            var detail = result.weatherforecast.pref[0].area[area].info[i].weather_detail[0] + "\n";
-            var max = "最高気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[0]._ + "度ハメ\n";
-            var min = "最低気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[1]._ + "度ですハメ";
-            
-            //今は青森だけど[TODO]
-            var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
+      if (!error && response.statusCode == 200) {
+        parseString(body, function (err, result) {
+        // 日付に一致する天気を取得。
+          for(let i=0; i < 6; i++) {
+            if(result.weatherforecast.pref[0].area[area].info[i]['$'].date === date){
+              console.log(date);
+              var day = result.weatherforecast.pref[0].area[area].info[i]['$'].date + "\n";
+              var weather = result.weatherforecast.pref[0].area[area].info[i].weather[0] + "\n";
+              var detail = result.weatherforecast.pref[0].area[area].info[i].weather_detail[0] + "\n";
+              var max = "最高気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[0]._ + "度ハメ\n";
+              var min = "最低気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[1]._ + "度ですハメ";
+
+              //今は青森だけど[TODO]
+              var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
+            }
           }
-        }
-      });
-    } else {
-      console.log(error + " : " + response);
-    }
+          resolve(tenki);
+        });
+      } else {
+        console.log(error + " : " + response);
+      }
+    });
   });
+  
+  return p;
 };
 
 // exports
