ed3d0b1ae0dfdc424e170694925322774276360a
Glitch (candle-nightshade) <none>
2019-08-30 06:06:34 +0000

    🐟🙈 Checkpoint ./main.js:6189558/215

--- main.js
+++ main.js
@@ -197,13 +197,13 @@ client.on('message', message =>
         message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
         return;
     }
-    //アローラオーキド博士 
-   if (message.content === 'アローラオーキド博士') {
+    //メガガイア
+  if (message.content.includes("@mega")) {
         console.log("オーキド博士");
         let channel = message.channel;
         let author = message.author.username;
         // そのチェンネルにメッセージを送信する
-        message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
+        message.reply("ｼﾞｬﾝｼﾞｬﾝﾊﾞﾘﾊﾞﾘ。。。",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
         return;
     }
 });
