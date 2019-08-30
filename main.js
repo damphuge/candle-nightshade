860986eafc65a0d95950f4b86a4f2ff8882681eb
Glitch (candle-nightshade) <none>
2019-08-30 05:54:34 +0000

    😜💓 Checkpoint ./main.js:6189558/404

--- main.js
+++ main.js
@@ -197,6 +197,15 @@ client.on('message', message =>
         message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
         return;
     }
+    //アローラオーキド博士 
+   if (message.content === 'アローラオーキド博士') {
+        console.log("オーキド博士");
+        let channel = message.channel;
+        let author = message.author.username;
+        // そのチェンネルにメッセージを送信する
+        message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
+        return;
+    }
 });
 
 if(process.env.DISCORD_BOT_TOKEN == undefined)
