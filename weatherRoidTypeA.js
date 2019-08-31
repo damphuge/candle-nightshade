80b5d74552c44edf96e7c978c15f551f6fb1be5a
Glitch (candle-nightshade) <none>
2019-08-31 22:45:22 +0000

    👵🍤 Checkpoint ./weatherRoidTypeA.js:6409424/5055

--- weatherRoidTypeA.js
+++ weatherRoidTypeA.js
@@ -1,28 +1,19 @@
 /*
   xmlファイルから天気情報を取得する関数
-  Promise使ってみたかった
-  
-  @param xml $body xmlデータ
-  @param String $date 日付(YYYY/MM/DD)
-  @param Integer $area 地域(初期値は青森県津軽を想定), xmlファイル内の地域id
-  @param String $pref 都道府県(初期値は青森県), xmlファイル名の番号
+
+  @param Array $result xmlデータ。特定日の天気情報が格納されている。
+  @return String $tenki ハメドリくんのご講説
 */
-let asyncParseString = function(body, date, area, pref) {
-  return new Promise(function (resolve, reject) {
-    const options = {
-      trim: true,
-      explicitArray: false
-    };
-    parseString(body, function (err, result) {
-    // 日付に一致する天気を取得。
-      for(let i=0; i < 6; i++) {
-        if(result.weatherforecast.pref[0].area[area].info[i]['$'].date === date){
-          resolve(result.weatherforecast.pref[0].area[area].info[i]);
-          } // if
-      } // for
-    }); // parseString()
-  }); // Promise function()
-} // asyncParseString()
+let povBird = function(result) {
+  let day = result['$'].date + "\n";
+  let weather = result.weather[0] + "\n";
+  let detail = result.weather_detail[0] + "\n";
+  let max = "最高気温は" + result.temperature[0].range[0]._ + "度ハメ\n";
+  let min = "最低気温は" + result.temperature[0].range[1]._ + "度ですハメ";
+  var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n";
+  tenki += day + weather + detail + max + min;      
+  return tenki;
+}
 
 /*
   天気を教える関数
@@ -39,7 +30,23 @@ let getWeather = function(date, area=2, pref='02'){
   //ここがXMLパーサ
   request (url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      result = await asyncParseString(body, date, area, pref);
+      let asyncParseString = function(body, date, area, pref) {
+        return new Promise(function (resolve, reject) {
+          const options = {
+            trim: true,
+            explicitArray: false
+          };
+          parseString(body, function (err, result) {
+          // 日付に一致する天気を取得。
+            for(let i=0; i < 6; i++) {
+              if(result.weatherforecast.pref[0].area[area].info[i]['$'].date === date){
+                resolve(result.weatherforecast.pref[0].area[area].info[i]);
+                } // if
+            } // for
+          }); // parseString()
+        }); // Promise function()
+      }(body, date, area, pref) // asyncParseString()
+      return povBird(asyncParseString);
     } else {
       console.log(error + " : " + response);
     }
@@ -51,13 +58,3 @@ let getWeather = function(date, area=2, pref='02'){
 module.exports = {
   getWeather: getWeather
 };
-
-/*var day = result.weatherforecast.pref[0].area[area].info[i]['$'].date + "\n";
-                  var weather = result.weatherforecast.pref[0].area[area].info[i].weather[0] + "\n";
-                  var detail = result.weatherforecast.pref[0].area[area].info[i].weather_detail[0] + "\n";
-                  var max = "最高気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[0]._ + "度ハメ\n";
-                  var min = "最低気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[1]._ + "度ですハメ";
-    */
-                  //今は青森だけど[TODO]
-                  //var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
-                
