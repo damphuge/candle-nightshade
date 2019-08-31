3400d23d5bc23c9a858c6cb69bc20e110e6ae15c
Glitch (candle-nightshade) <none>
2019-08-31 22:21:20 +0000

    🚣🎎 Checkpoint ./weatherRoidTypeA.js:6409424/616

--- weatherRoidTypeA.js
+++ weatherRoidTypeA.js
@@ -13,26 +13,34 @@ let getWeather = function(date, area=2, pref='02'){
     //ここがXMLパーサ
     request (url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
+      let asyncParseString = function(body) {
+        return new Promise(function (resolve, reject) {
+          const options = {
+            trim: true,
+            explicitArray: false
+          };
+        })
+      }
         parseString(body, function (err, result) {
         // 日付に一致する天気を取得。
           for(let i=0; i < 6; i++) {
             if(result.weatherforecast.pref[0].area[area].info[i]['$'].date === date){
-              
-              var day = result.weatherforecast.pref[0].area[area].info[i]['$'].date + "\n";
+              resolve(result.weatherforecast.pref[0].area[area].info[i])
+              /*var day = result.weatherforecast.pref[0].area[area].info[i]['$'].date + "\n";
               var weather = result.weatherforecast.pref[0].area[area].info[i].weather[0] + "\n";
               var detail = result.weatherforecast.pref[0].area[area].info[i].weather_detail[0] + "\n";
               var max = "最高気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[0]._ + "度ハメ\n";
               var min = "最低気温は" + result.weatherforecast.pref[0].area[area].info[i].temperature[0].range[1]._ + "度ですハメ";
-
+*/
               //今は青森だけど[TODO]
-              var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
+              //var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n" + day + weather + detail + max + min;
             }
           }
-          resolve(tenki);
         });
       } else {
         console.log(error + " : " + response);
       }
+
     });
   });
   
