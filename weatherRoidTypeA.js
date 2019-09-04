deec8b717dc5cad387435ce2e10cbaf94934d623
Glitch (candle-nightshade) <none>
2019-09-04 14:02:29 +0000

    🎄🌨 Checkpoint ./weatherRoidTypeA.js:6409424/11059

--- weatherRoidTypeA.js
+++ weatherRoidTypeA.js
@@ -1,71 +1,105 @@
-/*
-  ハメドリくんのテキストを生成するハメ。
+let request = require('request');
+let parseString = require('xml2js').parseString;
 
-  @param Promise $p 天気予報情報ハメ。マスかいてたくさん出てきたハメよ。
-  @return String $tenki 天気予報ハメ。マンコかっぽじってよく聞くハメ。
-*/
-let povBird = function(p) {
-  var tenki = "ハメドリくんだハメ。青森の天気予報だハメ\n";
-  p
-    .then((result)=> {
-      let day = result['$'].date + "\n";
-      let weather = result.weather[0] + "\n";
-      let detail = result.weather_detail[0] + "\n";
-      let max = "最高気温は" + result.temperature[0].range[0]._ + "度ハメ\n";
-      let min = "最低気温は" + result.temperature[0].range[1]._ + "度ですハメ";
-      tenki += day + weather + detail + max + min;
-      console.log('0' + (typeof tenki === 'undefined'));
-      console.log(tenki);
-      return tenki;
+/**
+ * Asyncanize request()
+ * 
+ * > nodejs requestモジュールをpromise化してasync/awaitで呼び出し
+ * > https://qiita.com/murase/items/908cf31b6776448a5b1d
+ * 
+ * @param {*} options URL and method
+ * @return Promise body
+ */
+let asyncRequest = (function (options) {
+  return new Promise(function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if(!error && response.statusCode == 200) {
+        resolve(body);
+      }
+      else {
+        reject(error);
+      }
+    });
   });
+});
+
+/**
+ * Asyncanize parseString()
+ * 
+ * > JavaScriptでasync/awaitに対応していない関数を対応させる
+ * > Living Absurd World
+ * > https://blog.hmatoba.net/Article/168
+ * 
+ * @param {*} body 
+ * @return Promise Array json
+ */
+let asyncParseString = (function (body) {
+  return new Promise(function (resolve, reject) {
+    // ここはわからん。写経した
+    const options = {
+      trim: true,
+      explicitArray: false
+    };
+    parseString(body, function (error, result) {
+      if(!error) {
+        resolve(result);
+      }
+      else {
+        reject(error);
+      }
+    });
+  });
+});
+
+/** 
+ * Get weather from json from xml (https://www.drk7.jp/weather/)
+ * 
+ * @param json weathers 1-week weather forecast for 1 pref.
+ * @param Date date YYYY/MM/DD
+ * @param Integer area 
+ * @param String pref 01-47
+ * @return json weather a day weather forecast for 1 region.
+ */
+async function getWeather(weathers, date, area, pref) {
+  for (let day=0; day<6; day++) {
+    if (weathers.weatherforecast.pref[pref].area[area].info[day]['$'].date === date) {
+      // resolveで日付一致した部分を返り値として渡している（はず）
+      return weathers.weatherforecast.pref[pref].area[area].info[day];
+  }
+}
+
+/**
+ * ハメドリくんのテキストを生成するハメ。
+ * 
+ * @param json weather お天気を受信するハメ！
+ * @return String singing ハメドリくんのさえずりハメ！パコパコハメ～！
+ */
+async function povBird(weather) {
+  var singing = "ハメドリくんだハメ。青森の天気予報だハメ\n";
+  let day = result['$'].date + "\n";
+  let sky = result.weather[0] + "\n";
+  let detail = result.weather_detail[0] + "\n";
+  let max = "最高気温は" + result.temperature[0].range[0]._ + "度ハメ\n";
+  let min = "最低気温は" + result.temperature[0].range[1]._ + "度ですハメ";
+  singing += day + sky + detail + max + min;
+
+  console.log('0' + (typeof singing === 'undefined'));
+  console.log(singing);
+
+  return singing;
+}
+
+async function TypeA(date, area=2, pref='02') {
+  const body = await asyncRequest(options);
+  const weathers = await asyncParseString(body);
+  const weather = await getWeather(weathers, date, area, pref);
+  const singing = await povBird(weather);
+  return singing;
 };
 
-/*
-  天気予報を教える関数
-  
-  @param String $date 日付(YYYY/MM/DD)
-  @param Integer $area 地域(初期値は青森県津軽を想定), xmlファイル内の地域id
-  @param String $pref 都道府県(初期値は青森県), xmlファイル名の番号
-  @return String povBird(asyncParseString) SCP-8585RECとの交信履歴
-*/
-let getWeather = function(date, area=2, pref='02'){
-  let request = require('request');
-  let parseString = require('xml2js').parseString;
-  let url = 'https://www.drk7.jp/weather/xml/' + pref + '.xml';
-  //ここがXMLパーサ
-  request (url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // JavaScriptでasync/awaitに対応していない関数を対応させる
-      // -- Living Absurd World
-      // https://blog.hmatoba.net/Article/168
-      let asyncParseString = (function(body, date, area, pref) {
-        return new Promise(function(resolve, reject){
-          // ここはわからん。写経した
-          const options = {
-            trim: true,
-            explicitArray: false
-          };
-          parseString(body, function (err, result) {
-            // 日付に一致する天気を取得。
-            for(let i=0; i < 6; i++) {
-              if(result.weatherforecast.pref[0].area[area].info[i]['$'].date === date){
-                // resolveで日付一致した部分を返り値として渡している（はず）
-                resolve(result.weatherforecast.pref[0].area[area].info[i]);
-              } // if
-            } // for
-          }); // parseString()
-        }); // Promise function()
-      })(body, date, area, pref); // asyncParseString() 即時関数にして実行
-      // ハメドリくんに解析結果を渡している（はず）
-      let hoge = povBird(asyncParseString);
-      console.log('2'+(typeof hoge === 'undefined'));
-      return hoge;
-    } else {
-      console.log(error + " : " + response);
-    }
-  }); // request()
-};
-
+let main = function(date, area=2, pref='02') {
+  return TypeA(date, area, pref);
+}
 /*
 function main(){
    console.log('RUN' + getWeather('2019/09/01'));
@@ -76,5 +110,5 @@ main();
 */
 // exports
 module.exports = {
-  getWeather: getWeather
+  main: main
 };
