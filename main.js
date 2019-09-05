aeeeec98b2d12aa9650ae16eaa46703c1fbf0615
Glitch (candle-nightshade) <none>
2019-09-05 07:57:53 +0000

    😬🎖 Checkpoint ./main.js:6189558/1344 ./sai.js:6189558/1409 ./.glitch-assets:6189558/2142

--- main.js
+++ main.js
@@ -156,19 +156,11 @@ if(message.isMemberMentioned(client.user))
         message.reply("text",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fokd.jpg?v=1567130070416", name: "okd.png" }]});
         return;
     }
-   // //アローラオーキド博士 
-   // if (message.content === 'アローラオーキド博士') {
-   //      console.log("オーキド博士");
-   //      let channel = message.channel;
-   //      let author = message.author.username;
-   //      // そのチェンネルにメッセージを送信する
-   //      message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
-   //      return;
-   //  }
+
   
-  //アローラ
+  //さいころv2
   var sai = require('./sai.js'); 
-     if (message.content === 'アローラオーキド博士') {
+     if (message.content === 'sai') {
        console.log("test");
         sai.test(message);
         return;
--- sai.js
+++ sai.js
@@ -2,14 +2,50 @@
 
  function test(message) {
         //アローラオーキド博士 
-   if (message.content === 'アローラオーキド博士') {
-        console.log("オーキド博士");
+
         let channel = message.channel;
         let author = message.author.username;
+        var num = Math.floor( Math.random() * 6 + 1 );
+        let reply_text = `${author}のさいころの出目は`+ num;
+        
+       
+       switch( num ) {
+
+
+    case 1:
+        console.log("1");
+        break;
+
+
+    case 2:
+        console.log("2");
+        break;
+           
+    case 3:
+        console.log("3");
+        break;
+
+
+    case 4:
+        console.log("4");
+        break;
+    
+    case 5:
+        console.log("5");
+        break;
+           
+    case 6: console.log("6");
+}
+   
+   
         // そのチェンネルにメッセージを送信する
-        message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
+        message.reply("text",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fokd.jpg?v=1567130070416", name: "okd.png" }]});
+
+        message.reply(reply_text)
+            .then(message => console.log(`Sent message: ${reply_text}`))
+            .catch(console.error);
         return;
-    }
+   
     }
 
 
