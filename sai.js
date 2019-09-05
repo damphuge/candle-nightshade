04653a85d4916d76e18523556a2d924e2f497d3c
Glitch (candle-nightshade) <none>
2019-09-05 08:09:53 +0000

    🐦🛬 Checkpoint ./sai.js:6189558/2689

--- sai.js
+++ sai.js
@@ -1,5 +1,5 @@
 
-
+var url;
  function test(message) {
         //アローラオーキド博士 
 
@@ -14,34 +14,44 @@
 
     case 1:
         console.log("1");
-        break;
+           url ="https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2F6d_01.gif?v=1567669845406";
 
+        break;
 
     case 2:
         console.log("2");
+           url ="https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2F6d_02.gif?v=1567669858976";
+
         break;
            
     case 3:
         console.log("3");
+ url ="https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2F6d_03.gif?v=1567669859424";
+
         break;
 
 
     case 4:
         console.log("4");
+          url ="https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2F6d_04.gif?v=1567669859234";
+
         break;
     
     case 5:
         console.log("5");
+         url ="https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2F6d_05.gif?v=1567669859465";
+
         break;
            
     case 6: console.log("6");
+     url ="https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2F6d_06.gif?v=1567669859180";
+
 }
    
    
         // そのチェンネルにメッセージを送信する
-        message.reply("text",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fokd.jpg?v=1567130070416", name: "okd.png" }]});
 
-        message.reply(reply_text)
+          message.reply("掴みとれ！",{files: [{ attachment:url, name: "sai.png" }]})
             .then(message => console.log(`Sent message: ${reply_text}`))
             .catch(console.error);
         return;
