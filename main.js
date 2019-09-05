eddde7d55eff6e037a64518001c3314e890b3cd2
Glitch (candle-nightshade) <none>
2019-09-05 07:45:57 +0000

    🎯🐩 Checkpoint ./sai.js:6189558/868 ./main.js:6189558/905

--- main.js
+++ main.js
@@ -156,19 +156,23 @@ if(message.isMemberMentioned(client.user))
         message.reply("text",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fokd.jpg?v=1567130070416", name: "okd.png" }]});
         return;
     }
-   //アローラオーキド博士 
-   if (message.content === 'アローラオーキド博士') {
-        console.log("オーキド博士");
-        let channel = message.channel;
-        let author = message.author.username;
-        // そのチェンネルにメッセージを送信する
-        message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
+   // //アローラオーキド博士 
+   // if (message.content === 'アローラオーキド博士') {
+   //      console.log("オーキド博士");
+   //      let channel = message.channel;
+   //      let author = message.author.username;
+   //      // そのチェンネルにメッセージを送信する
+   //      message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
+   //      return;
+   //  }
+  
+  //アローラ
+  var sai = require('./sai.js'); 
+     if (message.content === 'アローラオーキド博士') {
+       console.log("test");
+        sai.test(message);
         return;
     }
-  
-  
-  //var sai = require('./sai.js'); 
- 
 
   
   var info;
--- sai.js
+++ sai.js
@@ -1,3 +1,16 @@
-exports = {
-  
-};
+
+
+ function test(message) {
+        //アローラオーキド博士 
+   if (message.content === 'アローラオーキド博士') {
+        console.log("オーキド博士");
+        let channel = message.channel;
+        let author = message.author.username;
+        // そのチェンネルにメッセージを送信する
+        message.reply("ファッキュー・・・ぶち殺すぞ",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fa_okd.jpg?v=1567130077289", name: "okd.png" }]});
+        return;
+    }
+    }
+
+
+module.exports.test = test;
