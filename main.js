0a4dee37919273e78afe7e192ecddc4c123ac0cc
GGnara <aikokusyanokiwami@gmail.com>
2020-01-27 16:52:07 +0900

    メガガイア（前）削除

--- main.js
+++ main.js
@@ -186,44 +186,6 @@ client.on('message', message =>
             return;
         }
 
-
-        var info;
-        //メガガイア
-        if (message.content.includes("@mega")) {
-            console.log("メガガイア");
-            var mystr =message.content;
-            var result = mystr.replace(/[^0-9]/g, '');
-
-            let channel = message.channel;
-            let author = message.author.username;
-
-            // そのチェンネルにメッセージを送信する
-            var url ="http://hall.gaia-jp.com/sp/mb/playdatas/sdetail?st=aom&rb=S20&mno=" + result;
-
-            var image = "http://hall.gaia-jp.com/sp/gdrawmb.php?st=aom&dt=t&mno=" + result;
-
-            ch.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36';
-            ch.fetch(url, { q: 'node.js'}, function (err, $, res) {
-                 // レスポンスヘッダを参照
-                 // console.log(res.headers);
-
-                 // HTMLタイトルを表示
-                 //console.log($('title').text());
-                 info = $('b').text();
-                 //console.log($('b').text());
-                 console.log(info);
-                 message.reply(info+"だハメ！くそ台ハメねぇ",{files: [{ attachment: image, name: "okd.png" }]});
-            });
-
-            return;
-        }
-
-
-
-
-
-
-
         });
 
 if(process.env.DISCORD_BOT_TOKEN == undefined)
