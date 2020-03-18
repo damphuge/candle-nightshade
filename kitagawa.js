d00f503ae1e1b9a812556f224740811dd4708a11
konafx <afxlieonon@gmail.com>
2020-03-18 20:18:28 +0900

    Simplify kitagawa

--- kitagawa.js
+++ kitagawa.js
@@ -1,33 +1,27 @@
- function kitagawa_func(message,ch) {
+const random = require('random');
 
-   //きたがわ
-
-        let channel = message.channel;
-        let author = message.author.username;
-       
-        
-         
-      let reply_text = rand_text();
-
-   
-        // そのチェンネルにメッセージを送信する
-    message.reply(reply_text)
-            .then(message => console.log(`Sent message: ${reply_text}`))
-            .catch(console.error);
-            
-        return;
-    }
+function kitagawa_func(message,ch) {
+  //きたがわ
+  const channel = message.channel;
+  const author = message.author.username;
+  const reply_text = rand_text();
+  // そのチェンネルにメッセージを送信する
+  message.reply(reply_text)
+    .then(message => console.log(`Sent message: ${reply_text}`))
+    .catch(console.error);
+  return;
+}
 
 //ランダム文字列
 function rand_text() {
-  let msg;
-	let rand = Math.floor(Math.random()*5);
-	if (rand == 0) msg = "おめんど！";
-	if (rand == 1) msg = "うしこら！";
-	if (rand == 2) msg = "おめんど集合！";
-	if (rand == 3) msg = "おめんど最低だ！";
-	if (rand == 4) msg = "おめんどうしこら～！";
-	return msg
+  const serifs = [
+    "おめんど！",
+    "うしこら！",
+    "おめんど集合！",
+    "おめんど最低だ！",
+    "おめんどうしこら～！"
+  ];
+  return random.choice(serifs);
 }
 
 module.exports.kitagawa_func = kitagawa_func;
