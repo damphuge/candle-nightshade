691fafd146a2ff84a504be0ab56321a4e0a5d48e
konafx <afxlieonon@gmail.com>
2020-03-23 17:16:01 +0900

    Add machine

--- mega-osmosis.js
+++ mega-osmosis.js
@@ -1,8 +1,8 @@
 const osmosis = require('osmosis');
-function scrapePtag() {
+function fetchBounus() {
   return new Promise((resolve, reject) => {
     let results = [];
-    osmosis.get('http://hall.gaia-jp.com/sp/mb/playdatas/sdetail?st=aom&rb=S20&mno=1501')
+    osmosis.get(url)
       .find('p:has(b)')
       .set({
         name: 'text()',
@@ -15,16 +15,31 @@ function scrapePtag() {
       .done(() => resolve(results));
   });
 }
-scrapePtag()
-  .then(data => {
-    console.log(data)
-    console.log(data.find(element => element.name == 'BB'))
-  })
 
-function mega_func(message,ch) {
+function fetchMachineInfo(url) {
+  return new Promise((resolve, reject) => {
+    let result = {}
+    osmosis.get(url)
+      .find('div:has(h3 b)')
+      .set({
+        title: 'h3:skip(1)',
+        number: 'h3:last'
+      })
+      .data(item => result = item)
+      .done(() => resolve(result))
+  });
+}
+
+async function constructResText(url) {
+  const machine = await fetchMachineInfo(url)
+  const bounuses = await fetchBounus(url)
+  return `${machine.title}（${machine.number}）\nBB: ${bounuses.find(x => x.name == 'RB').time}回, RB: ${bounuses.find(x => x.name == 'RB').time}回`
+}
+
+function mega_osmosis(message, ch) {
   console.log("mega_new_runnnig");
 
-  const mystr =message.content;
+  const mystr = message.content;
   const result = mystr.replace(/[^0-9]/g, '');
 
   const channel = message.channel;
@@ -32,39 +47,14 @@ function mega_func(message,ch) {
 
   // そのチェンネルにメッセージを送信する
   const url ="http://hall.gaia-jp.com/sp/mb/playdatas/sdetail?st=aom&rb=S20&mno=" + result;
-
   const image = "http://hall.gaia-jp.com/sp/gdrawmb.php?st=aom&dt=t&mno=" + result;
 
-  ch.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36';
-  ch.fetch(url, {q: 'node.js'}, function(err, $, res){
-    // レスポンスヘッダを参照
-    let body;
-    //html抜き出し
-    body = $('body').html();
-
-    let name;
-
-    // HTMLタイトル（台名称）を表示
-    name = $('b').text();
-    test = $('p').text();
-
-    console.log(test)
-    //name形成関数
-    name = del_sp_br (name)
-    message.reply(name + " \n だハメ！どうせ改ざんハメ",{files: [{ attachment: image, name: "mega_data.png" }]});
+  let res_text = ''
+  constructResText(url).then((text) => {
+    res_text = text
   });
+  console.log(test)
+  message.reply(res_text + "\n だハメ！どうせ改ざんハメ",{files: [{ attachment: image, name: "mega_data.png" }]});
   return;
 }
-
-function del_sp_br (str){
-  //改行削除
-  str.replace(/\r?\n/g, '');
-
-  //空白削除
-  str = str.trim();
-
-  str = " "+str+" "
-    return str
-}
-// module.exports.mega_func = mega_func;
-
+module.exports.mega_func = mega_osmosis;
