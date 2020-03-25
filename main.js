942d576e4b5cdde05e02dd536874f9d9e1471277
g2 <g_nara@g-wise.local>
2020-03-25 15:15:07 +0900

    port変更

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ http.createServer(function(request, response)
     {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('Discord bot is active now \n');
-    }).listen(3000);
+    }).listen(process.env.PORT);
+
 
 // Discord bot implements
 const discord = require('discord.js');
