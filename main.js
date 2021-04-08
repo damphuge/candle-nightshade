1dbbefccc6f7b9cccf2c8f0af650f9caf0ba8d3f
konafx <afxlieonon@gmail.com>
2021-04-09 01:19:11 +0900

    Add Cog, Sensor
    Cog: コマンド
    Sensor: ワード反応型
    を作成、置き換えた。

--- main.js
+++ main.js
@@ -30,121 +30,317 @@ http.createServer(function(request, response)
     response.end('Discord bot is active now \n');
   }).listen(process.env.PORT || 8080);
 
-
 // Discord bot implements
 const discord = require('discord.js');
 const client = new discord.Client();
 var ch = require('cheerio-httpcli');
 
-//ここからBOTの反応
-client.on('ready', message =>
-  {
-    client.user.setPresence({ game: { name: 'ぬきたし２' } });
-    console.log('bot is ready!');
+const prefix = process.env.COMMAND_PREFIX || '!';
+
+/*
+ * @property callback function(message: discord.Message)
+ */
+class Cog {
+  constructor(command, callback, name, description, parameter) {
+    this.validate(command, callback);
+
+    this.command = command;
+    this.regex = new RegExp(`${prefix}${command}`);
+    this.callback = callback;
+    this.name = name;
+    this.description = description;
+    this.parameter = parameter;
+  }
+
+  validate(command, callback) {
+    if (!command) {
+      throw "command string is not defined"
+    }
+    if (typeof command != "string") {
+      throw `expected type 'string', found '${typeof command}'`
+    }
+    if (!callback) {
+      throw "callback function is not defined"
+    }
+    if (typeof callback != "function") {
+      throw `expected type 'function', found '${typeof callback}'`
+    }
+  }
+  get usage() {
+    return `${prefix}${this.command}` + (this.parameter ? ` ${this.parameter}` : '');
+  }
+}
+
+/*
+ * @property callback function(message: discord.Message)
+ */
+class Sensor {
+  constructor(name, words, callback, description) {
+    this.validate(name, words, callback);
+
+    this.name = name;
+    this.words = words;
+    this.callback = callback;
+    this.description = description;
+  }
+
+  validate(name, words, callback) {
+    if (!name) {
+      throw "name streing is not defined"
+    }
+    if (typeof name != "string") {
+      throw `expected type 'string', found '${typeof name}'`
+    }
+    if (!words) {
+      throw "command string[] is not defined"
+    }
+    if (!Array.isArray(words)) {
+      throw `expected type 'string[]', found '${typeof words}'`
+    }
+    if (typeof words[0] != "string") {
+      throw `expected type 'string[]', found '${typeof words[0]}[]'`
+    }
+    if (!callback) {
+      throw "callback function is not defined"
+    }
+    if (typeof callback != "function") {
+      throw `expected type 'function', found '${typeof callback}'`
+    }
+  }
+}
+
+const cogs = [];
+function addCog(command, callback, name='', description='', parameter='') {
+  cogs.push(new Cog(command, callback, name, description, parameter))
+}
+
+const sensors = [];
+function addSensor(words, callback, name='', description='') {
+  sensors.push(new Sensor(words, callback, name, description))
+}
+
+addCog("word", (message) => {
+  let index   = message.content.indexOf(" ");
+
+  // ４．基準文字列から後の文字列を切り出して表示 
+  let messe = message.content.slice(index + 1);        
+  const dbClient = new Client(dbConfig);
+
+  dbClient.connect()
+
+  dbClient
+    .query(`SELECT message FROM messages WHERE word = '${messe}' LIMIT 1`)
+    .then(res => {
+      if(res.rows.length == 0) {
+        message.reply(`そんな言葉は知らない. You should use \`!addword ${messe} <description>\`.`);
+        return;
+      }
+      message.channel.send(res.rows[0].message);
+    })
+    .catch(err => {
+      console.error(err);
+      message.reply(`エラーしたわ。`);
+      return;
+    })
+    .finally(()=> {
+      dbClient.end();
+    });
+});
+
+addCog("addword", (message) => {
+  const separations = `\\s\\r\\n`;
+  const matches = (new RegExp(`^${prefix}addword[${separations}]+([^${separations}]+)[${separations}]+([^]+)`))
+    .exec(message.content);
+  console.log(matches);
+  if (matches === null) {
+    console.error(`命令の形式にマッチしませんでした`);
+    return;
+  }
+
+  const keyWord = matches[1];
+  const keyDescription = matches[2];
+
+  const constraintName = 'messages_word_key';
+  const insertQuery = `INSERT INTO messages (word, message) VALUES ('${keyWord}', '${keyDescription}')
+          ON CONFLICT ON CONSTRAINT ${constraintName}
+          DO UPDATE SET message='${keyDescription}'`;
+  console.log(`SQL: ${insertQuery}`);
+
+  const dbClient = new Client(dbConfig)
+
+  dbClient.connect();
+  dbClient
+    .query(insertQuery)
+    .then(res => {
+      console.log(`Insert (${keyWord}: ${keyDescription})`);
+      message.reply(`追加しました. \`${prefix}word ${keyWord}\` で試してみてくれ`);
+    })
+    .catch(err => {
+      console.error(err);
+      message.reply(`エラーしたわ。原因は知らん`);
+    })
+    .finally(() => dbClient.end());
+},
+  null,
+  '※死ぬほど大きいAAには対応してないらしい。詳しくは俺もわからん',
+  '登録したいキーワード 登録したい説明文（改行可）'
+);
+
+addSensor("日本地図",
+  ["にほん"],
+  (message) => {
+    // そのチェンネルにメッセージを送信する
+    message.reply("",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fmap-japan-10210.png?v=1566197317736", name: "test.png" }]});
   });
 
+addSensor("お仕置き執行人津波両夢",
+  ["差別", "黒人", "朝鮮", "在日", "チョッパリ", "SS", "ナチ"],
+  (message) => {
+    console.log("差別検知");
+    // そのチェンネルにメッセージを送信する
+    message.reply("差別検知!!津波両夢は差別を許しません!",{files: [{ attachment: "https://cdn.discordapp.com/attachments/768317965800046602/827823207553040414/88808193_p0_master1200.jpg", name: "津波両夢.png" }]});
+  });
+
+addSensor("タイマー",
+  ["タイマー"],
+  (message) => {
+    console.log("time");
+    var str = message.content;
+    str.match(/([+-]?[0-9]+\.?[0-9]*)/g);
+    //setTimeout(countup, 1000);
+
+    let author = message.author.username;
+    // そのチェンネルにメッセージを送信する
+  });
+  
+addSensor("敗北者",
+  ["敗北者"],
+  (message) => {
+    if (message.isMemberMentioned(client.user) && message.member.voiceChannel) {
+      /* ボイスチャンネルにいる人がbotにメンションをしたときの処理 */
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          const fileNames = ["haibokusya.m4a", "torikeseyoimanokotoba.m4a"];
+          // 複数の音声データのうちいずれかをランダムで選ぶ
+          const fileName =
+            fileNames[Math.floor(Math.random() * fileNames.length)];
+
+          // 再生 再生終了時にボイスチャンネルから切断
+          const dispatcher = connection.playFile(fileName);
+          dispatcher.on("end", () => connection.disconnect());
+        })
+        .catch(console.error);
+    } else {
+      /* 敗北者という単語が含まれているが、
+                   メンションでなかったり発言者がボイスチャンネルにいないときの処理 */
+      const texts = [
+        "ハァ...ハァ...敗北者...?",
+        "取り消せよ...!!今の言葉...!!",
+      ];
+
+      // メンションする文字列をランダムで選択
+      const replyText = texts[Math.floor(Math.random() * texts.length)];
+      message.reply(replyText).catch(console.error);
+    }
+  });
+
+addSensor("オーキド博士",
+  ["オーキド博士"],
+  (message) => {
+    let author = message.author.username;
+    // そのチェンネルにメッセージを送信する
+    message.reply("text", { files: [
+      {
+        attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fokd.jpg?v=1567130070416",
+        name: "okd.png"
+      }
+    ]});
+  });
+
+addSensor("さいころ",
+  ["さいころ", "サイコロ"],
+  (message) => {
+    var sai = require('./sai.js');
+    sai.test(message);
+  });
+
+addCog("kita", (message) => {
+  var kitagawa = require('./kitagawa.js');
+  kitagawa.kitagawa_func(message,ch);
+},
+  "きたがわ");
+
+//ここからBOTの反応
+client.on('ready', message => {
+    client.user.setPresence({ game: { name: 'ぬきたし２' } });
+    console.log('bot is ready!');
+});
 
 client.on('message', message =>
   {
     // bot自身の発言は無視
     if (message.author.bot) return;
 
-    // よく使う変数はここに
-    const channel = message.channel;
+    if (/!help/.test(message.content)) {
+      const helpEmbedMessage = new discord.RichEmbed()
+      cogs
+        .forEach(cog => {
+          console.log(cog.command, cog.usage);
+          helpEmbedMessage.addField(cog.name || cog.command, cog.usage)
+        });
+      sensors
+        .forEach(sensor => helpEmbedMessage.addField(sensor.name, sensor.words.join(', ')));
+
+      helpEmbedMessage
+        .addField("天気の子", "今日, 明日, 明後日, 明々後日の天気")
+        .addField("メガガイア", "1234@mega, mega@1234（1234は機械番号）")
+
+      message.channel.send(helpEmbedMessage);
+    }
+
+    cogs.forEach(cog => {
+      if (cog.regex.test(message.content)) {
+        console.log(`START Cog: ${cog.command}`)
+        try {
+          cog.callback(message)
+        } catch (err) {
+          console.error(err);
+          if (cog.parameter) {
+            message.reply(cog.usage);
+          }
+        }
+        console.log(`END   Cog: ${cog.command}`)
+      }
+    });
+
+    let sensorResponsed = false;
+    sensors.forEach(sensor => {
+      if (sensorResponsed) {
+        return;
+      }
+      const content = message.content.replace(new RegExp(`<@!${client.user.id}>\\s+`), '')
+      if (sensor.words.includes(content)) {
+        console.log(`START Sensor: ${sensor.name}`)
+        try {
+          sensor.callback(message);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          sensorResponsed = true;
+          console.log(`END   Sensor: ${sensor.name}`)
+        }
+      }
+    });
+    if (sensorResponsed) {
+      return;
+    }
 
     //メンション
     if(message.isMemberMentioned(client.user))
     {
       message.reply( 'やりますねぇ！' );
       console.log("test");
-      return;
-    }
-
-    //!word
-    if(message.content.match(/^!word .*/)){
-      let index   = message.content.indexOf(" ");
-
-      // ４．基準文字列から後の文字列を切り出して表示 
-      let messe = message.content.slice(index + 1);        
-      const dbClient = new Client(dbConfig);
-
-      dbClient.connect()
-
-      dbClient
-        .query(`SELECT message FROM messages WHERE word = '${messe}' LIMIT 1`)
-        .then(res => {
-          if(res.rows.length == 0) {
-            message.reply(`そんな言葉は知らない. You should use \`!addword ${messe} <description>\`.`);
-            return;
-          }
-          channel.send(res.rows[0].message);
-        })
-        .catch(err => {
-          console.error(err);
-          message.reply(`エラーしたわ。`);
-          return;
-        })
-        .finally(()=> {
-          dbClient.end();
-        });
-    }
-    const addwordOperation = '!addword';
-    if((new RegExp(`^${addwordOperation}`)).test(message.content)) {
-      console.log(`${addwordOperation}命令`);
-      const separations = `\\s\\r\\n`;
-      const matches = (new RegExp(`^${addwordOperation}[${separations}]+([^${separations}]+)[${separations}]+([^]+)`))
-        .exec(message.content);
-      console.log(matches);
-      if (matches === null) {
-        console.log(`${addwordOperation} 命令の形式にマッチしませんでした`);
-        message.reply(
-          'usage: `!addword 登録したいキーワード 登録したい説明文（改行可）`\n' +
-          '※死ぬほど大きいAAには対応してないらしい。詳しくは俺もわからん'
-        );
-        return;
-      }
-
-      const keyWord = matches[1];
-      const keyDescription = matches[2];
-
-      const constraintName = 'messages_word_key';
-      const insertQuery = `INSERT INTO messages (word, message) VALUES ('${keyWord}', '${keyDescription}')
-          ON CONFLICT ON CONSTRAINT ${constraintName}
-          DO UPDATE SET message='${keyDescription}'`;
-      console.log(`SQL: ${insertQuery}`);
-
-      const dbClient = new Client(dbConfig)
-
-      dbClient.connect();
-      dbClient
-        .query(insertQuery)
-        .then(res => {
-          console.log(`Insert (${keyWord}: ${keyDescription})`);
-          message.reply(`追加しました. \`!word ${keyWord}\` で試してみてくれ`);
-        })
-        .catch(err => {
-          console.error(err);
-          message.reply(`エラーしたわ。原因は知らん`);
-        })
-        .finally(() => dbClient.end());
-      return;
-    }
-
-    //日本地図
-    if (message.content === 'にほん') {
-      console.log("にほん");
-      let author = message.author.username;
-      // そのチェンネルにメッセージを送信する
-      message.reply("text",{files: [{ attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fmap-japan-10210.png?v=1566197317736", name: "test.png" }]});
-      return;
-    }
-
-    //お仕置き執行人津波両夢
-    if (message.content === '差別' || message.content === '黒人' || message.content === '朝鮮' || message.content === '在日' || message.content === 'チョッパリ' || message.content === 'SS' || message.content === 'ナチ') {
-      console.log("差別検知");
-      let author = message.author.username;
-      // そのチェンネルにメッセージを送信する
-      message.reply("差別検知!!津波両夢は差別を許しません!",{files: [{ attachment: "https://cdn.discordapp.com/attachments/768317965800046602/827823207553040414/88808193_p0_master1200.jpg", name: "津波両夢.png" }]});
       return;
     }
 
@@ -176,80 +372,6 @@ client.on('message', message =>
         .catch(()=> { message.reply('長崎は今日も雨だった'); });
     }
 
-    //メッセージの文字列による条件分岐
-    if (message.content === 'タイマー') {
-
-      console.log("time");
-      var str = message.content;
-      str.match(/([+-]?[0-9]+\.?[0-9]*)/g);
-      //setTimeout(countup, 1000);
-
-      let author = message.author.username;
-      // そのチェンネルにメッセージを送信する
-      return;
-    }
-
-    //　敗北者という単語が含まれていたときの処理
-    if (message.content.includes(":test:")) {
-      if (message.isMemberMentioned(client.user) && message.member.voiceChannel) {
-        /* ボイスチャンネルにいる人がbotにメンションをしたときの処理 */
-        message.member.voiceChannel
-          .join()
-          .then(connection => {
-            const fileNames = ["haibokusya.m4a", "torikeseyoimanokotoba.m4a"];
-            // 複数の音声データのうちいずれかをランダムで選ぶ
-            const fileName =
-              fileNames[Math.floor(Math.random() * fileNames.length)];
-
-            // 再生 再生終了時にボイスチャンネルから切断
-            const dispatcher = connection.playFile(fileName);
-            dispatcher.on("end", () => connection.disconnect());
-          })
-          .catch(console.error);
-      } else {
-        /* 敗北者という単語が含まれているが、
-                   メンションでなかったり発言者がボイスチャンネルにいないときの処理 */
-        const texts = [
-          "ハァ...ハァ...敗北者...?",
-          "取り消せよ...!!今の言葉...!!",
-        ];
-
-        // メンションする文字列をランダムで選択
-        const replyText = texts[Math.floor(Math.random() * texts.length)];
-        message.reply(replyText).catch(console.error);
-      }
-    }
-
-    //usage
-    if (message.content === '!usage') {
-      let reply_text = 'さいころ, 天気, にほん, @timer, 敗北者, メンション, オーキド博士（アローラのすがたもあるぞい）';
-      message.reply(reply_text)
-        .then(message => console.log(`Sent message: ${reply_text}`))
-        .catch(console.error);
-    }
-    //オーキド博士
-    if (message.content === 'オーキド博士') {
-      console.log("オーキド博士");
-      let author = message.author.username;
-      // そのチェンネルにメッセージを送信する
-      message.reply("text", { files: [
-        {
-          attachment: "https://cdn.glitch.com/279b9a42-2a14-4625-a99c-585d8820443e%2Fokd.jpg?v=1567130070416",
-          name: "okd.png"
-        }
-      ]});
-      return;
-    }
-
-
-    //さいころv2
-    var sai = require('./sai.js');
-    if (message.content === 'さいころ') {
-      console.log("test");
-      sai.test(message);
-      return;
-    }
-
     //メガガイア
     var mega = require('./mega_osmosis.js');
     const megaList = ['@mega', 'mega@'];
@@ -257,13 +379,6 @@ client.on('message', message =>
     if(listIsMega.some(x=>x)){
       console.log("メガガイア");
       mega.mega_func(message,ch);
-      return;
-    }
-    //きたがわ
-    var kitagawa = require('./kitagawa.js');
-    if (message.content.includes("@kita")) {
-      console.log("きたがわ");
-      kitagawa.kitagawa_func(message,ch);
       return;
     }
 
