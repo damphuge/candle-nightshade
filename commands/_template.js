c789d5fb2e3f12d032317753c5300971a1c29ead
konafx <afxlieonon@gmail.com>
2021-04-16 18:55:25 +0900

    Add comments _template.js

--- commands/_template.js
+++ commands/_template.js
@@ -1,15 +1,32 @@
 'use strict';
 
+/**
+ * @var {string} name この名前で実行される
+ * @var {?string} description 説明
+ */
 const name = '_template';
 const description = 'This is template';
 
 /**
+ * 実行される関数
+ *
  * @param {Discord.Message} message
  * @param {Array.<string>} args
+ *
+ * @example <caption>args</caption>
+ * // メッセージ内容："!command foo bar"
+ * args==["foo", "bar"]
  */
 const execute = (message, args) => {
 };
 
+/**
+ * @var {?boolean} args 引数が必要か
+ * @var {?string} usage コマンド引数の説明
+ *
+ * @example <caption>usage</caption>
+ * '<arg1> <arg2>'
+ */
 module.exports = {
   name,
   description,
--- sensors/_template.js
+++ sensors/_template.js
@@ -1,13 +1,22 @@
 'use strict';
 
+/**
+ * @var {string} センサー名、適当で良い
+ * @var {?string} 説明
+ */
 const name = '_template';
 const description = 'This is template';
 
+/**
+ * @var {Array.<string>} words 反応する単語の列
+ */
 const words = [
   'Template', 'Example'
 ];
 
 /**
+ * 反応時の動作関数
+ *
  * @param {Discord.Message} message
  * @param {string} word マッチしたワード
  */
